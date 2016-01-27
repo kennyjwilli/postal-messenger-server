511c0fed900cf74a732b3e34a7763f12efcc1147
Kenny Williams <kennyjwilli@gmail.com>
2016-01-27 08:26:06 -0800

    Use pubnub

--- src/postal/server/handlers.clj
+++ src/postal/server/handlers.clj
@@ -4,10 +4,9 @@
             [catacumba.handlers.auth :as auth]
             [catacumba.handlers.postal :as pc]
             [catacumba.handlers.postal :as pc]
-            [clojure.core.async :refer [<! >! alts! chan put! timeout go go-loop close!] :as a]
+            [clojure.core.async :refer [<! >! alts! chan put! timeout go go-loop close!] :as async]
             [clojure.edn :as edn]
-            [amazonica.aws.sns :as sns]
-            [amazonica.aws.sqs :as sqs]
+            [pubnub :as p]
             [buddy.sign.jws :as jws]
             [postal.server.jwt :as jwt]
             [postal.server.util :as u]
@@ -15,11 +14,8 @@
             [pro.stormpath.auth :as sa]
             [pro.stormpath.account :as acc]
             [catacumba.core :as ct]
-            [clj-uuid :as uuid]
-            [cheshire.core :refer [parse-string]]
-            [postal.server.aws :as aws])
+            [cheshire.core :refer [parse-string]])
   (:import (ratpack.http TypedData)
-
            (ratpack.handling Context)))
 
 ;;===================================
@@ -33,6 +29,15 @@
 (def ^:private secret (System/getenv "JWT_RSA_PRV_PASS"))
 (def auth-backend
   (auth/jws-backend {:secret secret}))
+
+;;TODO: Replace with SQS
+(def message-relay (chan 100))
+
+(def creds {:access-key (System/getenv "AWS_ACCESS_KEY_ID")
+            :secret-key (System/getenv "AWS_SECRET_ACCESS_KEY")})
+
+(def pubnub-creds {:subscribe-key (System/getenv "SUBSCRIBE_KEY")
+                   :publish-key   (System/getenv "PUBLISH_KEY")})
 
 ;;===================================
 ;; PARSING
@@ -65,9 +70,8 @@
         account (:account auth-result)]
     (if (:success auth-result)
       ;; TODO: Should use JWE?
-      (http/ok (jwt/sign {:username  username
-                          :topic-arn (:topic-arn (sns/create-topic (u/alpha-numberic username)))
-                          :roles     (acc/get-group-names account)} secret))
+      (http/ok (jwt/sign {:username username
+                          :roles    (acc/get-group-names account)} secret))
       (http/unauthorized "Incorrect login"))))
 
 (defmulti postal-handler
@@ -83,53 +87,29 @@
           (success-fn decoded)))
       (error-fn "No token"))))
 
-(defn random-queue-name-for
-  [username]
-  (str (u/alpha-numberic username) "-" (uuid/v1)))
+(defn- channel-for
+  [user]
+  (-> {:channel user} (merge pubnub-creds) p/channel))
 
-(defn sqs-queue-for!
-  [username sns-arn]
-  (let [queue-name (random-queue-name-for username)]
-    (sqs/create-queue :queue-name queue-name
-                      :attributes {:Policy (aws/allow-sns-push-to-sqs-policy (aws/sqs-arn queue-name) sns-arn)})))
-
-(defn sqs-message-chan
-  ([url timeout]
-   (let [chan (chan 10)]
-     (go-loop []
-       (when (<! chan)
-         (let [messages (:messages (sqs/receive-message :queue-url url
-                                                        :max-number-of-messages 1
-                                                        :delete true))]
-           (when-not (empty? messages)
-             (doseq [msg messages]
-               (>! chan (parse-string (:body msg) true)))))
-         (<! (a/timeout timeout))
-         (recur)))
-     chan))
-  ([url] (sqs-message-chan url 500)))
+(defn- send-message
+  [ch msg]
+  (go (>! ch (pc/frame {:value msg}))))
 
 (defmethod postal-handler {:type :socket
                            :dest :messages}
   [ctx frame]
   (letfn [(on-connect [{:keys [in out ctrl] :as context}]
             (println "connect")
+            ;; TODO: Make all async
             (ws-auth
               frame
               (fn [identity]
-                (let [topic-arn (:topic-arn identity)
-                      queue-url (-> identity :username (sqs-queue-for! topic-arn) :queue-url) ;TODO: Normalize identity
-                      queue-arn (-> queue-url sqs/get-queue-attributes :QueueArn)
-                      sqs-chan (sqs-message-chan queue-url)]
-                  (println "CREATE SQS" queue-arn)
-                  (sns/subscribe :topic-arn topic-arn
-                                 :protocol "sqs"
-                                 :endpoint queue-arn)
-                  (println "SUBSCRIBED")
+                (let [channel (channel-for (:username identity))
+                      subscription (p/subscribe channel)]
                   (go-loop []
-                    (let [[v p] (alts! [ctrl in sqs-chan])]
+                    (let [[v p] (alts! [ctrl in subscription])]
                       (cond
-                        ;; RECEIVE
+                        ;; RECEIVE FROM CLIENT
                         (= p in)
                         (do
                           (println "IN" v)
@@ -138,17 +118,19 @@
                             (close! out))
                           (recur))
 
-                        ;; RECEIVE FROM SQS
-                        (= p sqs-chan)
+                        ;; RECIEVE FROM PUBNUB
+                        (= p subscription)
                         (do
-                          (println "SQS" v)
+                          (println "v" v)
+                          (when (= (:status v) :success)
+                            (send-message out (:payload v)))
                           (recur))
 
                         ;; CLOSE
                         (= p ctrl)
                         (do
                           (println "Channel closed")
-                          (sqs/delete-queue queue-url))))))
+                          #_(sqs/delete-queue queue-url))))))
                 (go
                   (>! out (pc/frame {:value "test"}))))
               (fn [err-msg]
